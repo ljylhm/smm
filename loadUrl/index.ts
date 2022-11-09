20296c0f64c3bb0b438221882633355c4aee58b7
kitions <nsplknsplk@qq.com>
2022-11-09 11:24:52 +0800

    格式化代码

--- loadUrl/index.ts
+++ loadUrl/index.ts
@@ -1,23 +1,23 @@
-import { LoadUrl,NECaptcha,Type } from "smmloadurl"
+import { LoadUrl, NECaptcha, Type } from "smmloadurl"
 
 const btn_1 = document.querySelector("#btn-1") as HTMLDivElement
 const btn_2 = document.querySelector("#btn-2") as HTMLDivElement
 const btn_3 = document.querySelector("#btn-3") as HTMLDivElement
 const btn_4 = document.querySelector("#btn-4") as HTMLDivElement
 
-let neCap:NECaptcha
-let neCap1:NECaptcha
+let neCap: NECaptcha
+let neCap1: NECaptcha
 
-btn_1.addEventListener("click",()=>{
+btn_1.addEventListener("click", () => {
 
-    LoadUrl.loadurl("http://cstaticdun.126.net/load.min.js",{
-        success(){
+    LoadUrl.loadurl("http://cstaticdun.126.net/load.min.js", {
+        success() {
             console.log("成功返回")
             // 初始化一个云盾的实例
             neCap = new NECaptcha({
-                NECapOpt:{
-                    element:".container-box",
-                    onVerify(err,data){
+                NECapOpt: {
+                    element: ".container-box",
+                    onVerify(err, data) {
                         console.log(neCap.NECaptchaInfo)
                         neCap.NECaptchaInfo.validate = data.validate
                     }
@@ -25,9 +25,9 @@ btn_1.addEventListener("click",()=>{
             })
 
             neCap1 = new NECaptcha({
-                NECapOpt:{
-                    element:".container-box-1",
-                    onVerify(err,data){
+                NECapOpt: {
+                    element: ".container-box-1",
+                    onVerify(err, data) {
                         console.log(neCap1.NECaptchaInfo)
                         neCap1.NECaptchaInfo.validate = data.validate
                     }
@@ -35,17 +35,17 @@ btn_1.addEventListener("click",()=>{
             })
 
         },
-        error(){
+        error() {
             console.log("发生了错误")
         },
-        isBefore:false,
+        isBefore: false,
         type: Type.defer
     })
 })
 
-btn_2.addEventListener("click",()=>{
+btn_2.addEventListener("click", () => {
     neCap.refresh()
 })
-btn_4.addEventListener("click",()=>{
+btn_4.addEventListener("click", () => {
     neCap1.refresh()
 })
